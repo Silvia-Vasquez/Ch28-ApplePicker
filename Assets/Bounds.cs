5df3e194a5d1a929c49dcd07fca1884ba89fbf2b
Silvia Vasquez <falcon19@scarletmail.rutgers.edu>
2019-09-26 19:15:49 -0400

    Added tree, apple, bounds, dropping, moving

---
+++ Assets/Bounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Apple")
+          Destroy(collision.gameObject);
+    }
+
+
+}
---
+++ Assets/Tree.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public float treeSpeed = 2.0f;
+
+    public GameObject prefabApple;
+    void Start()
+    {
+        Invoke("DropApple",2.0f);   
+    }
+
+    void DropApple()
+    {
+        GameObject Apple = Instantiate < GameObject>(prefabApple);
+        Apple.transform.position = transform.position;
+        Invoke("DropApple", 1.0f);
+            }
+
+    void Update()
+    {
+        transform.Translate(treeSpeed * Time.deltaTime, 0, 0);
+      
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Boundries")
+            treeSpeed *= -1.0f;
+    }
+
+
+
+}
